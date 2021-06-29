16fdd96226b3d6dc15e74b040a73ed4ac75f72b1
João Paulo <mg_ses@hotmail.com>
2021-06-29 16:04:25 -0300

    uri problems/problem 2611 solved

---
+++ uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.uri2602;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uri2602Application implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uri2602Application.class, args);
+	}
+}
---
+++ uri2611/src/main/java/com/devsuperior/uri2611/Uri2611Application.java
@@ -0,0 +1,43 @@
+package com.devsuperior.uri2611;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.devsuperior.uri2611.dto.MovieMinDTO;
+import com.devsuperior.uri2611.projections.MovieMinProjection;
+import com.devsuperior.uri2611.repositories.MovieRepository;
+
+@SpringBootApplication
+public class Uri2611Application implements CommandLineRunner{
+	
+	@Autowired
+	private MovieRepository movieRepository;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Uri2611Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		System.out.println("SQLraiz");
+		List<MovieMinProjection> searchRaiz = movieRepository.searchRaiz("Action");
+		System.out.println("\n");
+		List<MovieMinDTO> listDTO = searchRaiz.stream().map(x -> new MovieMinDTO(x)).collect(Collectors.toList());
+		listDTO.forEach(x -> System.out.println(x.toString()));
+		
+		System.out.println("\n\n");
+		
+		System.out.println("JPQL");
+		List<MovieMinProjection> searchJpql = movieRepository.searchJpql("Action");
+		System.out.println("\n");
+		List<MovieMinDTO> listDTO1 = searchJpql.stream().map(x -> new MovieMinDTO(x)).collect(Collectors.toList());
+		listDTO1.forEach(x -> System.out.println(x.toString()));
+		
+	}
+}
